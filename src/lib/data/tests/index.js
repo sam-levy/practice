68025d535c069a9f295795958bbb1689cecf635c
Samuel Levy <samulevy@gmail.com>
2026-08-04 09:34:10 -0300

    add test to index

--- src/lib/data/tests/index.js
+++ src/lib/data/tests/index.js
@@ -9,5 +9,10 @@ export const testManifest = [
     title: "P2 PEDIATRIA T9A/B SBC 2026.1",
     file: () => import("./p2_pediatria_t9ab_sbc_2026_1.json"),
   },
+  {
+    id: "p1_ped_t5b_17_11_2023",
+    title: "P1 PED - T5B - 17/11/2023",
+    file: () => import("./p1_ped_t5b_17_11_2023.json"),
+  },
   // Add more tests here later, following the same shape
 ];
